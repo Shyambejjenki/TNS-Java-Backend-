6a70b3b439bcc553e04c9ba4b3a917148fa073a5
Shyambejjenki <shyamreddy5525@gmail.com>
2025-08-16 20:01:46 +0530

    initial commit

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+public class Forloop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=10 ; i++)   
+		{	
+			System.out.print("Value of i is :");
+			System.out.println(i);
+		}
+	}
+}
+
+    
+
