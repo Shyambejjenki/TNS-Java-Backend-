0c4f157b680d9cd9645b0797284ad7f83663943b
shyamreddy <shyamreddy5525@gmail.com>
2025-08-16 19:18:20 +0530

    intial commit

---
+++ if_else_.java
@@ -0,0 +1,13 @@
+public class if_else_ {
+	public static void main(String[] args) {
+		int age=15;
+		
+		if(age>=18)
+			System.out.println(" You are an Adult");	
+		else
+			System.out.println("You are an a Teenager");
+
+	}
+
+}
+
