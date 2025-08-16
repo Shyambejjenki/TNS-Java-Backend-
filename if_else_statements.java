da94ea01e1731c9e6b20db72045fc482ae44c14f
shyamreddy <shyamreddy5525@gmail.com>
2025-08-16 19:28:40 +0530

    initial commit

---
+++ if_else_statements.java
@@ -0,0 +1,22 @@
+//Demo for taking input from user using Scanner
+import java.util.Scanner;
+public class if_else_statements {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+
+        // Take input
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // If-else condition
+        if (age >= 18) {
+            System.out.println("You are eligible to vote!");
+        } else {
+            System.out.println(" OOH!Sorry, you are not eligible to vote.");
+        }
+
+        sc.close(); // Close Scanner
+    }
+}
+    
