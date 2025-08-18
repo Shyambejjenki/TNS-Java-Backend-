91768bd5fa2b5a6e81b35bb53594e8905c068cda
ShyamBejjenki <shyambejjenki@gmail.com>
2025-08-18 06:31:27 +0530

    initial commit

--- tns/oopconcepts/encapsulation/employee.java
+++ tns/oopconcepts/encapsulation/employee.java
@@ -1,1 +1,40 @@
+package tns.oopconcepts.encapsulation;
 
+public class employee {
+    // Private fields for encapsulation
+    private String name;
+    private int age;
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+    
+    public static void main(String[] args) {
+        employee employee = new employee();
+
+        // Using setter methods to set values
+        employee.setName("shyam");
+        employee.setAge(30);
+        
+        // Using getter methods to retrieve values
+        System.out.println("employee's Name: " + employee.getName());
+        System.out.println("emplyee's Age: " + employee.getAge());
+    
+    }
+}
