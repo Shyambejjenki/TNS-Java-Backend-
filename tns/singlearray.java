61564ca69e55609f9ee285a62908d8c84e081c82
ShyamBejjenki <shyambejjenki@gmail.com>
2025-08-19 23:30:00 +0530

    initial commit

---
+++ tns/singlearray.java
@@ -0,0 +1,12 @@
+public class singlearray {
+    
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        System.out.println(a.length);
+        for(int row=0;row<a.length;row++)
+        {
+        System.out.print(a[row]);
+        }
+        
+    }
+}
