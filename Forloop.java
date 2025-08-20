09260867ca5ca7fa892bc4ed38add843f80da12d
ShyamBejjenki <shyambejjenki@gmail.com>
2025-08-20 22:47:22 +0530

    initial commit

--- Forloop.java
+++ Forloop.java
@@ -1,7 +1,7 @@
 public class Forloop {
 	public static void main(String[] args) 
 	{	
-		for(int i = 1; i<=10 ; i++)   
+		for(int i = 1; i<=12 ; i++)   
 		{	
 			System.out.print("Value of i is :");
 			System.out.println(i);
