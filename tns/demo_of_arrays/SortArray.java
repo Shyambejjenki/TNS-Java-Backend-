8b548a11d65d18d16a8a1ba7324eeff777e58bc8
ShyamBejjenki <shyambejjenki@gmail.com>
2025-08-20 22:16:14 +0530

    initial commit

---
+++ tns/demo_of_arrays/SortArray.java
@@ -0,0 +1,8 @@
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 7, 1};
+        Arrays.sort(arr);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+    }
+}
+
